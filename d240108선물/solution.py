c674232d829d9e88511761cf17ab29f0f283ca41
sunsik17 <sunsig21c@gmail.com>
2024-01-08 20:50:56 +0900

    백준 선물 문제 풀이

--- d240108선물/solution.py
+++ d240108선물/solution.py
@@ -3,14 +3,15 @@ N, L, W, H = map(int, input().split())
 
 start = 0
 end = 1_000_000_000
-
-for i in range(100):
+count = 0
+while count < 100:
   mid = (start + end) / 2
   if (L // mid) * (W // mid) * (H // mid) >= N:
     start = mid
   else:
     end = mid
+  count += 1
 
-print(f'{end: .15f}')
+print(f'{end: .10f}')
 
 
