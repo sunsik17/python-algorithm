cea7fd6822640aa57e9b09dea3e962ddd6d7c02d
sunsik17 <sunsig21c@gmail.com>
2024-01-11 14:26:32 +0900

    백준 문제 소가 길을 건너간 이유1 풀이
    python 단순 구현

---
+++ d240111소가길을건너간이유1/solution.py
@@ -0,0 +1,16 @@
+n = int(input())
+result = [-1 for _ in range(10)]
+answer = 0
+for _ in range(n):
+    cow, index = map(int, input().split())
+
+    if result[cow] == -1:
+        result[cow] = index
+        continue
+
+    if result[cow] != index:
+        answer += 1
+        result[cow] = index
+
+
+print(answer)
