9751daab02696c884b78af32d6f164ec6fe01ab1
sunsik17 <sunsig21c@gmail.com>
2024-01-22 17:03:55 +0900

    프로그래머스 최솟값만들기 문제 풀이

---
+++ d240122최솟값만들기/solution.py
@@ -0,0 +1,10 @@
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort()
+    l = len(B) - 1
+    for i in range(len(A)):
+        answer = answer + (A[i] * B[l])
+        l -= 1
+
+    return answer
